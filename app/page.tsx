570f01d0a75c1b5773041d358f94f940e1af1bdc
Logan534857345973 <Loganenemy@gmail.com>
2025-03-05 14:43:43 +0300

    qapwq

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
-import { Title } from "@radix-ui/react-dialog";
-import { Container } from "postcss";
+import Image from "next/image";
+import { CheckboxFiltersGroup, Container, Filters, Title, TopBar} from "@/components/shared/";
+
+
 
 export default function Home() {
   return (
-    <>
-      <Title/>
-    </>
+    <div>
+      <Container className="mt-10">
+        <Title size="lg" text="Все пиццы" className="font-black"></Title>
+
+      </Container>
+      <TopBar/>
+      <Container>
+        <div>
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          <div>
+            
+          </div>
+        </div>
+      </Container>
+
+    </div>
   );
 }
--- components/shared/checkbox-filters-group.tsx
+++ components/shared/checkbox-filters-group.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 
 import React from "react";
 import { FilterCheckbox, FilterCheckboxProps } from "./filter-checkbox";
@@ -17,7 +17,7 @@ interface Props {
     className?: string;
 }
 
-export const CheckboxFiltersGroup: React.FC<Props> = ({ 
+export const CheckboxFiltersGroup: React.FC<Props> = ({
     title,
     items,
     defaultItems,
@@ -26,7 +26,7 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
     className,
     onChange,
     defaultValue,
- }) => {
+}) => {
     const [showAll, setShowAll] = React.useState(false);
 
     const [searchValue, setSearchValue] = React.useState('');
@@ -35,23 +35,46 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
         setSearchValue(e.target.value);
     }
 
-const list = showAll
-    ? items.filter((item) => items.text.toLowerCase().includes(searchValue.toLowerCase()))
-    : defaultItems.slice(0, limit);
+    const list = showAll
+        ? items.filter((item) => item.text.toLowerCase().includes(searchValue.toLowerCase()))
+        : defaultItems.slice(0, limit);
     return (
         <div className={className}>
             <p className="font-bold mb-3">{title}</p>
 
             {showAll && (
                 <div className="mb-5">
-                    <Input 
+                    <Input
                         onChange={onChangeSearchInput}
                         placeholder={searchInputPlaceholder}
                         className="bg-gray-50 border-none"
+
                     />
+                </div>
+
+
+            )}
+
+            <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
+                {list.map((item, index) => (
+                    <FilterCheckbox
+                        onCheckedChange={(ids) => console.log(ids)}
+                        checked={false}
+                        key={index}
+                        value={item.value}
+                        text={item.text}
+                        endAdornment={item.endAdornment}
+                    />
+                ))}
+            </div>
+
+            {items.length > limit && (
+                <div className={showAll ? 'border-t border-t-neutral-100 mt-4' : ''}>
+                    <button onClick={() => setShowAll(!showAll)} className="text-primary mt-3">
+                        {showAll ? 'скрыть' : '+ Показать все'}
+                    </button>
                 </div>
             )}
         </div>
-
     );
-}
+};
